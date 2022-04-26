80345e3000ba53472c89c6231353913e5b23b336
4d4x <r40444@gmail.com>
2022-04-26 05:53:49 +0100

    initial commit

---
+++ src/main/java/com/adaxlab/jmix/rys/screen/user/UserBrowse.java
@@ -0,0 +1,12 @@
+package com.adaxlab.jmix.rys.screen.user;
+
+import com.adaxlab.jmix.rys.entity.User;
+import io.jmix.ui.navigation.Route;
+import io.jmix.ui.screen.*;
+
+@UiController("rys_User.browse")
+@UiDescriptor("user-browse.xml")
+@LookupComponent("usersTable")
+@Route("users")
+public class UserBrowse extends StandardLookup<User> {
+}
